c13330c511790590068fd351917cbf351fc22bfc
JP <joaopaulovs2000@gmail.com>
2023-08-18 19:23:47 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleEstado() {
+    let card3 = document.getElementById('card3')
+    let card4 = document.getElementById('card4')
+    let card5 = document.getElementById('card5')
+    let card6 = document.getElementById('card6')
+
+    let verMaisVerMenos = document.getElementById('verMaisVerMenos')
+
+    card3.classList.toggle('hide')
+    card4.classList.toggle('hide')
+    card5.classList.toggle('hide')
+    card6.classList.toggle('hide')
+
+    if (card3.classList == 'hide') {
+        verMaisVerMenos.textContent = 'Ver mais'
+    } else {
+        verMaisVerMenos.textContent = 'Ver menos'
+    }
+}
